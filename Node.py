08fe7bbd049ad43e1cebb0abb5cce321bb0873cd
zchoi22 <88288951+zchoi22@users.noreply.github.com>
2022-05-10 21:27:59 -0600

    remade node class for python

---
+++ Node.py
@@ -0,0 +1,44 @@
+
+class Node:
+    #constructor, has option values for left and right nodes
+    def __init__(self, key, value, size, left=None, right=None):
+        self.key = key
+        self.value = value
+        self.size = size
+        self.left = left
+        self.right = right
+
+    #To string method
+    def toString(self):
+        return "Node{" + "key=" + self.key + ", value=" + self.value + "}"
+
+    #getters and setters for all variables
+    def getKey(self):
+        return self.key
+
+    def setKey(self, key):
+        self.key = key
+
+    def getValue(self):
+        return self.value
+
+    def setValue(self, value):
+        self.value = value
+
+    def setLeft(self, left):
+        self.left = left
+
+    def setRight(self, right):
+        self.right = right
+
+    def getLeft(self):
+        return self.left
+
+    def getRight(self):
+        return self.right
+
+    def getSize(self):
+        return self.size
+
+    def setSize(self, size):
+        self.size = size
